9cf77ed7a80590d12066a82ffc0d03ca3bca296c
Tung Tran <56991132+C2H4Br2@users.noreply.github.com>
2022-11-21 18:17:30 +0700

    Initial layout for Login

---
+++ src/authentication/Login.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+import "../index.css";
+import background from "../bg-1.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Login() {
+  const bgStyle = {
+    backgroundImage: `url(${background})`,
+    height: "100vh",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat"
+  };
+  return (
+    <Container fluid style={bgStyle}>
+      <Row className="vh-100 d-flex justify-content-center align-items-center">
+        <Col md={8} lg={6} xs={12} />
+        <Col md={6} lg={4} xs={8}>
+          <Card className="shadow">
+            <Card.Body>
+              <div className="mb-3 mt-md-4 mx-4">
+                <h2 className="fw-bold mb-4" style={{ textAlign: "center" }}>
+                  Login
+                </h2>
+                <div className="mb-3">
+                  <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <Form.Label
+                        className="text-center"
+                        style={{ fontWeight: "bold" }}
+                      >
+                        Email address
+                      </Form.Label>
+                      <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                      <Form.Label
+                        className="text-center"
+                        style={{ fontWeight: "bold" }}
+                      >
+                        Password
+                      </Form.Label>
+                      <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                      <p className="small">
+                        <a className="text-primary" href="#!">
+                          Forgot password?
+                        </a>
+                      </p>
+                    </Form.Group>
+                    <div className="d-grid">
+                      <Button variant="primary" type="submit">
+                        Login
+                      </Button>
+                    </div>
+                  </Form>
+                  <div className="mt-3">
+                    <p className="mb-0  text-center">
+                      Don&apos;t have an account?{" "}
+                      <a href="{''}" className="text-primary fw-bold">
+                        Sign Up
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Login;

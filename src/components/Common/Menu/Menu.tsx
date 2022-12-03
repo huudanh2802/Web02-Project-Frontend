0d1fab5a45a3e3ca2806f4b7bfdd7756a2e626fa
Tung Tran <56991132+C2H4Br2@users.noreply.github.com>
2022-12-03 13:33:40 +0700

    Revamped Menu bar

--- src/components/Common/Menu/Menu.tsx
+++ src/components/Common/Menu/Menu.tsx
@@ -1,88 +1,52 @@
-/* eslint-disable */
-import * as fa from "react-icons/fa";
-
-import {  Nav, Navbar,Image, Col, Container, Row } from "react-bootstrap";
-import ListGroup from "react-bootstrap/ListGroup";
-
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { MenuData } from "./MenuData";
-import "./Menu.css";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { IconContext } from "react-icons";
+import {
+  Nav,
+  Navbar,
+  Image,
+  Col,
+  Container,
+  Row,
+  Button
+} from "react-bootstrap";
 
 import "./Menu.css";
 
-export default function Menu  ()  {
-  const [user,setUser]=useState("");
-  // Routing
+function Menu() {
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    const localEmail=localStorage.getItem('email');
-    if(localEmail){
-      setUser(localEmail);      
-    }  
-  },[])
+  const resetLocalStorage = () => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("token");
+    localStorage.removeItem("id");
+    console.log("Local storage reset");
+    navigate("/login");
+  };
+
   return (
-    <>
-    <Container className="menu-container"  >
+    <Container className="menu-container">
       <Row>
-      <Navbar className="navbar">
-      <IconContext.Provider value={{ color: "#fff" }}>
-        <Col lg={2} style={{height:"inherit"}}>
-          <Image src="/assets/kahot-logo.png" className="logo"/>
-        </Col>
-        <Col lg={7} style={{height:"inherit"}}>
-        <ListGroup className={"nav-menu"}>
-              {MenuData.map((item, index) => {
-                return (
-                  <ListGroup.Item
-                    key={index}
-                    className={item.cName}
-                    style={{backgroundColor: "#318f9b", border: "#318f9b"}}
-                  >
-                    <Link to={item.path}>
-                      <p className="router-icon" style={{ textAlign: "center" }}> 
-                      {item.icon}
-                      </p>
-
-                    </Link>
-                  </ListGroup.Item>
-                );
-              })}
-            </ListGroup>
-        </Col>
-            <Col lg={3} style={{height:"inherit"}}>
-            <Nav
-                className="ms-auto"
-                style={{ display: "flex", flexDirection: "row", marginTop:"10px ",  alignItems: "center" ,justifyContent:"flex-end"}}
-              >
-
-                <Nav  style={{ color: "white", alignItems: "center" , paddingLeft: "8px"}}>
-                  <img
-                    src="/assets/profileAvatar.png"
-                    style={{ width: 30, height: 30, marginRight: "3px", marginBottom: "3px" }}
-                  />
-                  {user}
-                </Nav>
-                <Nav.Link 
-                  onClick={() => {
-                    localStorage.removeItem("email");
-                    localStorage.removeItem("token");
-                    localStorage.removeItem("id");
-                    navigate("/login");
-                  }} 
-                  style={{ color: "white", textDecoration: "underline" }}>
-                  Logout
-                </Nav.Link>
-              </Nav>
-            </Col>
-    </IconContext.Provider>
-    </Navbar>
+        <Navbar className="navbar">
+          <Col lg={2} style={{ height: "inherit" }}>
+            <Image src="/assets/kahot-logo.svg" className="logo" />
+          </Col>
+          <Col lg={7} style={{ height: "inherit" }} />
+          <Col lg={3} style={{ height: "inherit" }}>
+            <Nav className="menu-end">
+              <img
+                src="/assets/avatar.svg"
+                alt="Avatar"
+                style={{ width: 32, height: 32, marginRight: "16px" }}
+              />
+              <Button variant="light" onClick={() => resetLocalStorage()}>
+                Logout
+              </Button>
+            </Nav>
+          </Col>
+        </Navbar>
       </Row>
     </Container>
-    </>
   );
-};
+}
 
+export default Menu;
